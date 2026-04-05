072a8673dc500f28b4599b3bacbf2dd3121644cc
AmritK-umar <amritk00singh@gmail.com>
2026-04-06 02:11:57 +0530

    fix: next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,21 @@ const nextConfig = {
     UPSTASH_REDIS_REST_URL: process.env.UPSTASH_REDIS_REST_URL,
     UPSTASH_REDIS_REST_TOKEN: process.env.UPSTASH_REDIS_REST_TOKEN,
   },
+  async headers() {
+    return [
+      {
+        // This applies the CORS headers to all your API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          // Allow your local frontend to connect
+          { key: "Access-Control-Allow-Origin", value: "http://localhost:3000" }, 
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT,OPTIONS" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  }
 }
 
 export default nextConfig
